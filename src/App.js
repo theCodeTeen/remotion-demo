02d522fb6cf3203d1fb0957108034b5137519dfa
Dharmik <whatsapp83trending@gmail.com>
2024-07-24 18:56:41 +0530

    peter wang made default

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Player } from "@remotion/player";
 import {MyVideo} from './MyVideo';
 
 const App = () => {
-  const [name,setName] = useState("");
+  const [name,setName] = useState("Peter Wang");
   const playerRef = useRef();
 
   const onInputValueChange = (e)=>{
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <div style={{padding: '20px'}}>
       <h1>Remotion demo for </h1>
-      <div style={{display:"flex",marginBottom:"50px",cursor:"pointer"}}><input type='text' placeholder='Peter Wang' onChange={onInputValueChange} style={{height:"30px", fontSize:"25px"}}/><div onClick={onSubmitAndPlayHandler} style={{height:"30px", padding:"3px 10px",fontSize:"16px",backgroundColor:"black",color:"white",display:"flex",alignItems:"center",justifyContent:"center"}}>Submit and Play</div></div>
+      <div style={{display:"flex",marginBottom:"50px",cursor:"pointer"}}><input value={name} type='text' placeholder='Peter Wang' onChange={onInputValueChange} style={{height:"30px", fontSize:"25px"}}/><div onClick={onSubmitAndPlayHandler} style={{height:"30px", padding:"3px 10px",fontSize:"16px",backgroundColor:"black",color:"white",display:"flex",alignItems:"center",justifyContent:"center"}}>Submit and Play</div></div>
       <Player
         component={MyVideo}
         durationInFrames={90}
